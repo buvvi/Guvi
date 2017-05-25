eabbdbf4b4f5a63ade265d2955209106c9c47708
Ramprasath M <raoramprasath@gmail.com>
2017-05-25 16:22:15 +0530

    Create while.php

---
+++ while.php
@@ -0,0 +1,25 @@
+<!--Increment the value of i at least once, and it will continue incrementing the variable i as long as it has a value of less than 10
+
+-->
+
+
+<?php 
+ 
+
+
+
+
+$i = 0;
+$j = 9;
+
+while($i <$j)
+{
+
+	$i++;
+echo"i value ".$i."<br>";
+
+
+
+}
+
+?>
