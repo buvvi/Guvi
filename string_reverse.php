2ef077dbe9eee1bc7c6fd9a9b8e3b2ae6357d052
Ramprasath M <raoramprasath@gmail.com>
2017-05-25 10:54:34 +0530

    Create string_reverse.php

---
+++ string_reverse.php
@@ -0,0 +1,23 @@
+<!-- Write php program to reverse a string  -->
+
+
+<?php
+
+$a="ramprasath";
+
+
+echo"givn string is ramprasath<bcompiler_read(filehandle)> ";
+//string length
+
+echo "The given string length is ".strlen($a)."<br>";
+
+//string Reverse
+
+echo "String Reverse ".strrev($a)."<br>";
+
+//string word_count
+echo "String word count  ".str_word_count($a)."<br>";
+
+?>
+
+
