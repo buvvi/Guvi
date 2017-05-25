5eb97bda0b1ce5e1d6dca4058a577d06611e9d82
Ramprasath M <raoramprasath@gmail.com>
2017-05-25 11:06:06 +0530

    Create substring.php

---
+++ substring.php
@@ -0,0 +1,18 @@
+<!-- How to find the last occurrence of a substring in a string
+
+  -->
+
+
+<?php
+
+$a="ramprasath ram ram";
+
+
+echo"givn string is ramprasath ram ram<br> ";	
+//string length
+
+echo "the last occurrence of a substring in a string ".strripos($a,"ram")."<br>";
+
+
+
+?>
