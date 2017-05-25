4b19fe7debb2ab202410560610c1471411e147ff
Ramprasath M <raoramprasath@gmail.com>
2017-05-25 13:30:11 +0530

    Create natural.php

---
+++ natural.php
@@ -0,0 +1,15 @@
+<!--calculate the sum of natural numbers -->
+
+
+<?php
+
+$start = 1;
+$end = 10;
+
+$sum = 0;
+for ($i = $start; $i <= $end; $i++) {
+    $sum += $i;
+}
+
+echo "Sum from " . $start . " to " . $end . " = " . $sum;
+?>	
